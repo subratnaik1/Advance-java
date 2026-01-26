9a43010ccce673ec08973055f988258389b1fcb4
subratnaik1 <subratnaik2008@gmail.com>
2026-01-26 16:32:25 +0530

    Callablestatement

--- AdvanceJavaJdbc/src/com/pack1/JdbcPro8.java
+++ AdvanceJavaJdbc/src/com/pack1/JdbcPro8.java
@@ -3,12 +3,26 @@ package com.pack1;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Types;
 
 public class JdbcPro8 {
 	String driver = "oracle.jdbc.OracleDriver";
 	String dbUrl = "jdbc:oracle:thin:@localhost:1521:orcl";
 	String dbUname = "subrat";
 	String dbPwd = "67890";
+
+	Connection connect() {
+		Connection con = null;
+		try {
+			IO.println("connecting to database");
+			Class.forName(driver);
+			con = DriverManager.getConnection(dbUrl, dbUname, dbPwd);
+			IO.println("connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 
 	void m1() {
 		IO.println("implementing callable statement");
@@ -34,10 +48,53 @@ public class JdbcPro8 {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
+
+	void m2() {
+		IO.println("implementing callable statement===>procedure2");
+		try {
+			Connection con = connect();
+			CallableStatement cstmt = con.prepareCall("{call retriveEmpData(?,?,?,?,?)}");
+			String e_id = IO.readln("enter id");
+			cstmt.setString(1, e_id);
+			cstmt.registerOutParameter(2, Types.VARCHAR);
+			cstmt.registerOutParameter(3, Types.VARCHAR);
+			cstmt.registerOutParameter(4, Types.NUMERIC);
+			cstmt.registerOutParameter(5, Types.NUMERIC);
+			cstmt.execute();
+			IO.println("---------employee details-------");
+			IO.println("employee id " + e_id);
+			IO.println("employee name " + cstmt.getString(2));
+			IO.println("employee desg " + cstmt.getString(3));
+			IO.println("employee basic sal " + cstmt.getInt(4));
+			IO.println("employee total sal " + cstmt.getInt(5));
+		} catch (Exception e) {
+			IO.println(e.getMessage());
+		}
+	}
+    void m3() {
+		IO.println("implementing callable statement===>functions");
+		try {
+			Connection con=connect();
+			CallableStatement cstmt =con.prepareCall("{call ?:=RetriveTsal(?)}");
+			String e_id = IO.readln("enter id");
+			cstmt.setString(2, e_id);
+			cstmt.registerOutParameter(1, Types.NUMERIC);
+			cstmt.execute();
+			IO.println("---------employee details-------");
+			IO.println("employee id" + e_id);
+			IO.println("employee total sal" + cstmt.getInt(1));
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+    }
 
 	void main() {
 		JdbcPro8 j8 = new JdbcPro8();
 		j8.m1();
+		//j8.m2();
+		//j8.m3();
 	}
 }
